6e6947a04efd57fd2c0dce4cde7d6a185cc73bd6
Andresb22 <150817768+Andresb22@users.noreply.github.com>
2024-05-15 18:23:13 -0300

    Add files via upload

---
+++ Tutorial-de-HTML/JS/app.js
@@ -0,0 +1,2 @@
+console.log("Hola, estamos aprendiendo HTML")/*muestra un mensaje por consola, al impesionar la pagina*/
+/*alert("Hola Mundo") muestra un mensaje en pantalla al ingresar a la pagina*/  
